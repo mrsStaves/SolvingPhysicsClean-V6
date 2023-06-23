10b8629aa1812d1832993a6bf14c7946d74188e9
Mrs Staves <estaves@st-francis.herts.sch.uk>
2023-06-23 16:46:23 +0100

    Initial Commit

---
+++ SolvingPhysicsClean/SolvingPhysicsCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolvingPhysicsCleanApp.swift
+//  SolvingPhysicsClean
+//
+//  Created by SFCMacMini1 on 23/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SolvingPhysicsCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
